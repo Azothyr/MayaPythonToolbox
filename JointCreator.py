489e25fa4f008a71e88e53b8ddf611eaba495aa7
Zac Peterson <Peterson.Zac17@gmail.com>
2023-02-08 21:00:07 -0700

    joint tool UI working checkpoint

--- JointCreator.py
+++ JointCreator.py
@@ -1,4 +1,5 @@
 import maya.cmds as cmds
+from functools import partial
 
 
 def create_ui():
@@ -44,9 +45,9 @@ def create_ui():
     cmds.menuItem(label='User Input')
 
     cmds.columnLayout(adjustableColumn=True, parent='first_column')
-    cmds.checkBox('checkbox_center', label='Create at Center of Mass', value=False)
-    cmds.checkBox('checkbox_parent', label='SParent Objects on Creation', value=False)
-    cmds.checkBox('checkbox_freeze', label='Freeze Transforms on Completion', value=False)
+    checkbox_center = cmds.checkBox(label='Create at Center of Mass', value=False)
+    checkbox_parent = cmds.checkBox(label='SParent Objects on Creation', value=False)
+    checkbox_freeze = cmds.checkBox(label='Freeze Transforms on Completion', value=False)
 
     cmds.rowColumnLayout('creation_order_columns', enable=True, numberOfColumns=2,
                          columnAttach=(1, 'right', 0),
@@ -54,47 +55,54 @@ def create_ui():
                          adjustableColumn=True)
     cmds.text(label='Creation Order')
     cmds.columnLayout('order_column', adjustableColumn=True, parent='creation_order_columns')
-    cmds.radioCollection('order_collection', parent='order_column')
+    order_control = cmds.radioCollection()
     rb1 = cmds.radioButton(label='First Object Selected to Last')
     rb2 = cmds.radioButton(label='Last Object Selected to First')
 
-    cmds.radioCollection('order_collection', edit=True, select=rb1)
-
-    scheme_input = cmds.textField(name_input, query=True)
-    center_bool = cmds.checkBox('checkbox_center', query=True)
-    parent_bool = cmds.checkBox('checkbox_parent', query=True)
-    freeze_bool = cmds.checkBox('checkbox_freeze', query=True)
-    creation_order = cmds.radioCollection('order_collection', query=True)
+    order_control = cmds.radioCollection(order_control, edit=True, select=rb1)
 
     cmds.columnLayout(adjustableColumn=True, rowSpacing=10, parent='first_column')
-    cmds.button(label='Execute', command=create_joints(selections,
-                                                       scheme_input,
-                                                       center_bool,
-                                                       parent_bool,
-                                                       freeze_bool,
-                                                       creation_order))
+    cmds.button(label='Execute', command=partial(pass_values,
+                                                 selections,
+                                                 name_input,
+                                                 checkbox_center,
+                                                 checkbox_parent,
+                                                 checkbox_freeze,
+                                                 order_control))
 
     cmds.showWindow(ui_window)
 
 
-def create_joints(selections, naming_scheme, center_bool, parent_bool, freeze_bool, creation_order):
-    """
-    Creates a joint at each selection(s) transform.
-    Returns: [joints]
-    """
-    new_joints = []
+def pass_values(select_list, scheme_input, center_bool, parent_bool, freeze_bool, order_control):
+    selections = select_list
+    scheme_input = cmds.textField(scheme_input, query=True, text=True)
+    center_bool = cmds.checkBox(center_bool, query=True, value=True)
+    parent_bool = cmds.checkBox(parent_bool, query=True, value=True)
+    freeze_bool = cmds.checkBox(freeze_bool, query=True, value=True)
+    creation_order = cmds.radioCollection(order_control, query=True, select=True)
+    tool_manager(selections, scheme_input, center_bool, parent_bool, freeze_bool, creation_order)
 
-    for selection in selections:
-        position = cmds.xform(selection, query=True, translation=True, worldSpace=True)
-        cmds.select(clear=True)
 
-        jnt = cmds.joint()
-        new_joints.append(jnt)
-        cmds.xform(jnt, worldSpace=True, translation=position)
+def tool_manager(selections, naming_scheme, center_bool, parent_bool, freeze_bool, creation_order):
+    def create_joints():
+        """
+        Creates a joint at each selection(s) transform.
+        Returns: [joints]
+        """
+        new_joints = []
 
-    cmds.select(new_joints, replace=True)
+        if center_bool:
+            for selection in selections:
+                position = cmds.xform(selection, query=True, translation=True, worldSpace=True)
+                cmds.select(clear=True)
 
-    return new_joints
+                jnt = cmds.joint()
+                new_joints.append(jnt)
+                cmds.xform(jnt, worldSpace=True, translation=position)
+
+            cmds.select(new_joints, replace=True)
+    
+            return new_joints
 
 
 create_ui()
