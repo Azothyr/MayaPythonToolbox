ca0be7b30ac4b8ba1a6ee79fa11d5a3fe6e89ed0
Zac Peterson <Peterson.Zac17@gmail.com>
2023-05-02 04:48:43 -0600

    small update

--- Scratch.py
+++ Scratch.py
@@ -4,43 +4,41 @@ selected_obj = cmds.ls(selection=True)[0]
 
 # Keyframe 1
 cmds.currentTime(0)
+cmds.setAttr(selected_obj + '.rz', 35)
 cmds.setKeyframe(selected_obj)
 
 # Keyframe 2
 cmds.currentTime(15)
-cmds.setAttr(selected_obj + '.rz', 35)
+cmds.setAttr(selected_obj + '.rz', -35)
 cmds.setKeyframe(selected_obj)
 
 # Keyframe 3
 cmds.currentTime(30)
-cmds.setAttr(selected_obj + '.rz', -35)
+cmds.setAttr(selected_obj + '.rz', 0)
 cmds.setKeyframe(selected_obj)
 
 # Keyframe 4
 cmds.currentTime(45)
-cmds.setAttr(selected_obj + '.rz', 0)
+cmds.setAttr(selected_obj + '.ry', 35)
 cmds.setKeyframe(selected_obj)
 
 # Keyframe 5
 cmds.currentTime(60)
-cmds.setAttr(selected_obj + '.rx', 35)
+cmds.setAttr(selected_obj + '.ry', -35)
 cmds.setKeyframe(selected_obj)
 
 # Keyframe 6
 cmds.currentTime(75)
-cmds.setAttr(selected_obj + '.rx', -35)
+cmds.setAttr(selected_obj + '.ry', 0)
 cmds.setKeyframe(selected_obj)
 
 # Keyframe 7
 cmds.currentTime(90)
-cmds.setAttr(selected_obj + '.rx', 0)
+cmds.setAttr(selected_obj + '.tx', 2)
 cmds.setKeyframe(selected_obj)
 
 # Keyframe 8
 cmds.currentTime(105)
-cmds.setAttr(selected_obj + '.tx', 2)
 cmds.setKeyframe(selected_obj)
 
-# Keyframe 9
-cmds.currentTime(120)
-cmds.setKeyframe(selected_obj)
+cmds.currentTime(0)
--- ScriptsInMaya/ManualToolRunner.py
+++ ScriptsInMaya/ManualToolRunner.py
@@ -16,3 +16,48 @@ for attr in attrs:
 cmds.setAttr(selected_obj + '.translate', 0, 0, 0)
 cmds.setAttr(selected_obj + '.rotate', 0, 0, 0)
 cmds.setAttr(selected_obj + '.scale', 1, 1, 1)
+
+import maya.cmds as cmds
+
+selected_obj = cmds.ls(selection=True)[0]
+
+# Keyframe 1
+cmds.currentTime(0)
+cmds.setAttr(selected_obj + '.rz', 35)
+cmds.setKeyframe(selected_obj)
+
+# Keyframe 2
+cmds.currentTime(15)
+cmds.setAttr(selected_obj + '.rz', -35)
+cmds.setKeyframe(selected_obj)
+
+# Keyframe 3
+cmds.currentTime(30)
+cmds.setAttr(selected_obj + '.rz', 0)
+cmds.setKeyframe(selected_obj)
+
+# Keyframe 4
+cmds.currentTime(45)
+cmds.setAttr(selected_obj + '.ry', 35)
+cmds.setKeyframe(selected_obj)
+
+# Keyframe 5
+cmds.currentTime(60)
+cmds.setAttr(selected_obj + '.ry', -35)
+cmds.setKeyframe(selected_obj)
+
+# Keyframe 6
+cmds.currentTime(75)
+cmds.setAttr(selected_obj + '.ry', 0)
+cmds.setKeyframe(selected_obj)
+
+# Keyframe 7
+cmds.currentTime(90)
+cmds.setAttr(selected_obj + '.tx', 2)
+cmds.setKeyframe(selected_obj)
+
+# Keyframe 8
+cmds.currentTime(105)
+cmds.setKeyframe(selected_obj)
+
+cmds.currentTime(0)
